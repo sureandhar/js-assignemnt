1c2fdeb4b560bd9ac0e72f9faa078e99c648c090
sureandhar <40564824+sureandhar@users.noreply.github.com>
2020-07-20 11:07:21 +0530

    js assignment

--- Day 4/asjs 1.js
+++ Day 4/asjs 1.js
@@ -1,26 +1,42 @@
 //console.log('hello')
+
 let i,s;
-for(i=1;i<=15;i++)
+for(i=1;i<=100;i++)
 {
 
+    let d=0;
     if(i%3==0)
     {
         s=i;
         //console.log(i);
+        if(i%5==0 && i%3==0)
+        {
+            // console.log('fizbuzz ' + i);
+            d=1;
+        }
+        // console.log("fiz " +i);
+    }
+   
+
+    if(d==1)
+    {
+        console.log('fizzbuzz ' +i);
+    }
+    else{
+        if(i%3==0)
+        {
+            console.log('fizz ' +i)
+        }
+
         if(i%5==0)
         {
-            console.log('fizbuzz ' + i);
+            console.log('buzz ' +i)
+
         }
-        console.log("fiz " +i);
 
     }
 
-    if(i%5==0)
-    {
-        console.log('buzz '+i);
     
-
-    }
 }
 
 
--- Day 4/asjs5.js
+++ Day 4/asjs5.js
@@ -16,16 +16,17 @@ if(inp>0 && inp<=5000)
 }
 else if(inp > 5000 && inp<=10000)
 {
-    a=inp/100;
-    a=a*5;
+    
+    let c=inp-5000;
+    a=(5000*0.02)+(c*0.05);
     // console.log(a);
 
 
 }
 else if(inp>10000 && inp<20000)
 {
-    a=inp/100;
-    a=a*7;
+    let d=inp-10000;
+    a=(5000*0.02)+(5000*0.05)+(d*0.07);
     // console.log(a);
 
 }
